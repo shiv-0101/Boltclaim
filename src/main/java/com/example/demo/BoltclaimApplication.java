8e2be4dc4885a72bd43d2961af44569d90dde66c
shiv-0101 <shivendrathakur321@outlook.com>
2026-02-11 23:50:16 +0530

    update application class with servlet initializer

--- src/main/java/com/example/demo/BoltclaimApplication.java
+++ src/main/java/com/example/demo/BoltclaimApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class BoltclaimApplication {
+public class BoltclaimApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BoltclaimApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoltclaimApplication.class, args);
